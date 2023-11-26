d7a868ba36ff53e8b25afe682a4fe8bf533aaed9
ShinHeeJin <sisesu92@gmail.com>
2023-11-26 21:33:28 +0900

    Optional parameters

--- query_parameters.py
+++ query_parameters.py
@@ -7,6 +7,17 @@ app = FastAPI()
 async def read_item(skip: int = 0, limit: int = 10):
     """
     Request: http://127.0.0.1:8000/items/?skip=1&limit=4
-    Response : [1, 4]
+    Response: [1, 4]
     """
     return skip, limit
+
+
+@app.get("/items/{item_id}")
+async def read_item2(item_id: str, q: str | None = None):
+    """
+    Request: http://127.0.0.1:8000/items/test
+    Response: {"item_id": "test", "q": null}
+    """
+    if q is not None:
+        return {"q": q}
+    return {"item_id": item_id, "q": q}
