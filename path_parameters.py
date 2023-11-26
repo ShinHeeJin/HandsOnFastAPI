bd1d4ab1c576323ace76b8d4cf8ff9c5c912c95f
ShinHeeJin <sisesu92@gmail.com>
2023-11-26 21:25:41 +0900

    Path parameters containing paths

--- path_parameters.py
+++ path_parameters.py
@@ -12,6 +12,7 @@ class ModelName(Enum):
 app = FastAPI()
 
 
+# Using Enum
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     print(f"{type(model_name)}")  # <enum 'ModelName'>
@@ -22,3 +23,13 @@ async def get_model(model_name: ModelName):
         return "model_name is lenet"
 
     return ["model_name is resnet"]
+
+
+# Using Path by `:path` ( like file path )
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    """
+    Request : http://127.0.0.1:8000/files//home/user
+    Response : {"file_path": "/home/user"}
+    """
+    return {"file_path": file_path}
