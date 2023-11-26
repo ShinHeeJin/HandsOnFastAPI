c788febd62020b2969ad3c60783ca0ad7c5e7a1c
ShinHeeJin <sisesu92@gmail.com>
2023-11-26 23:11:18 +0900

    body data using Pydantic BaseModel

--- 4_request_body.py
+++ 4_request_body.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None  # Use None to make it just optional.
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def a():
-    return
+# body data using Pydantic BaseModel
+@app.post("/items/")
+async def create_item(item: Item):
+    """
+    Request json : {"name": "Foo","price": 45.3}
+    Response json : {"name": "Foo","description": null,"price": 45.3,"tax": null}
+    """
+    item_dict = item.model_dump()
+    print(f"item_dict : {item_dict}")
+    if item.tax:
+        price = item.price + item.tax
+        item_dict.update({"price_with_tax": price})
+    return item_dict
