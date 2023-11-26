e27be25f92a33a44ffb0745ab30ecfd66c85561a
ShinHeeJin <sisesu92@gmail.com>
2023-11-26 23:16:32 +0900

    Request body + path + query parameters

--- 4_request_body.py
+++ 4_request_body.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 # body data using Pydantic BaseModel
 @app.post("/items/{item_id}")
-async def create_item(item: Item, item_id: int):
+async def create_item(item: Item, item_id: int, q: str | None = None):
     """
     Request json : {"name": "Foo","price": 45.3}
     Response json : {"name": "Foo","description": null,"price": 45.3,"tax": null}
@@ -24,4 +24,5 @@ async def create_item(item: Item, item_id: int):
     if item.tax:
         price = item.price + item.tax
         item_dict.update({"price_with_tax": price})
+    item_dict.update({"q": q})
     return item_dict
