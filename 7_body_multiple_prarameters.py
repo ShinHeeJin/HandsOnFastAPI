3edc7efa8017381a92776b951984852cef281b25
ShinHeeJin <sisesu92@gmail.com>
2023-11-27 21:48:45 +0900

    Multiple body parameters

--- 7_body_multiple_prarameters.py
+++ 7_body_multiple_prarameters.py
@@ -13,12 +13,17 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+class User(BaseModel):
+    user_name: str
+    full_name: str | None = None
+
+
 # Mix Path, Query and body parameters
 @app.put("/items/{item_id}")
 async def update_item(
     item_id: Annotated[int, Path(title="The ID of the item", ge=0, le=1000)],
     q: str | None = None,
-    item: Item | None = None,
+    item: Item | None = None,  # optional
 ):
     result = {"item_id": item_id}
     if q:
@@ -26,3 +31,24 @@ async def update_item(
     if item:
         result |= {"item": item}
     return None
+
+
+# Multiple body parameters
+@app.put("/items2/{item_id}")
+async def update_item2(item_id: int, item: Item, user: User):  # required item & user
+    """
+    Request Example
+    {
+        "item": {
+            "name": "Foo",
+            "description": "The pretender",
+            "price": 42.0,
+            "tax": 3.2
+        },
+        "user": {
+            "username": "dave",
+            "full_name": "Dave Grohl"
+        }
+    }
+    """
+    return {"item_id": item_id, "item": item, "user": user}
