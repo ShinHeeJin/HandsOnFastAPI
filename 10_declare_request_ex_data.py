608fefa2b5b6b6db523b6a07f0ec703c0aa97ec7
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 00:03:56 +0900

    Using the openapi_examples Parameter

--- 10_declare_request_ex_data.py
+++ 10_declare_request_ex_data.py
@@ -24,8 +24,9 @@ class Item(BaseModel):
     }
 
 
-# Extra JSON Schema data in Pydantic models
-# Body with multiple examples
+# 1. Extra JSON Schema data in Pydantic models
+# 2. Body with multiple examples
+# 3. Using the openapi_examples Parameter
 @app.put("/items/{item_id}")
 async def update_item(
     item_id: int,
@@ -47,7 +48,34 @@ async def update_item(
                     "name": "Baz",
                     "price": "thirty five point four",
                 },
-            ]
+            ],
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
         ),
     ],
 ):
