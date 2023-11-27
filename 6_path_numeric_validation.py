1de6dfb547ba2253eb2db549ba6b5d1911d203cb
ShinHeeJin <sisesu92@gmail.com>
2023-11-27 18:29:09 +0900

    Number validations: floats, greater than and less than

--- 6_path_numeric_validation.py
+++ 6_path_numeric_validation.py
@@ -24,3 +24,18 @@ async def read_items2(item_id: Annotated[int, Path(title="The ID of the item", g
     if query:
         result |= {"query": query}
     return result
+
+
+# Number validations: floats, greater than and less than
+@app.get("/items3/{item_id}")
+async def read_items3(
+    *,
+    query: str,
+    size: Annotated[float, Query(gt=0, lt=10.5)],
+    item_id: Annotated[int, Path(title="The ID of the item", ge=0, le=1000)],
+):
+    result = {"item_id": item_id}
+    result = result | {"query": query}
+    result |= {"size": size}
+    print
+    return result
