55940ceefbd583e5a7bad37eec445384747b67f6
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 00:35:50 +0900

    Duplicate headers

--- 13_header_parameters.py
+++ 13_header_parameters.py
@@ -9,3 +9,9 @@ app = FastAPI()
 @app.get("/items/")
 async def read_items(user_agent: Annotated[str | None, Header()] = None):
     return {"User-Agent": user_agent}
+
+
+# Duplicate headers
+@app.get("/items2/")
+async def read_items2(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-Token values": x_token}
