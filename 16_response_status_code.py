d3926fc7d364241c614a4b9c7b0751bfa92323dd
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 16:43:31 +0900

    Shortcut to remember the names

--- 16_response_status_code.py
+++ 16_response_status_code.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
@@ -8,4 +8,10 @@ app = FastAPI()
 # Response Status Code
 @app.post("/items/", status_code=HTTPStatus.CREATED)
 async def create_item(name: str):
+    return {"name": name}
+
+
+# Shortcut to remember the names
+@app.post("/items2/", status_code=status.HTTP_201_CREATED)
+async def create_item2(name: str):
     return {"name": name}
