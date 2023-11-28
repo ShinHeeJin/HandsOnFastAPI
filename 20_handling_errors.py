1415c2a67dac985b2519c6160cfee952e1bcedfd
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 18:10:54 +0900

    Install custom exception handlers

--- 20_handling_errors.py
+++ 20_handling_errors.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 items = {"foo": "The Foo Wrestlers"}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(status_code=418, content={"message": f"Opps! {exc.name} did something"})
 
 
 # Use HTTPException & Add custom headers
@@ -13,3 +24,11 @@ async def read_item(item_id: str):
             status_code=404, detail={"message": "Item not found"}, headers={"X-Error": "There goes my error"}
         )
     return {"item": items[item_id]}
+
+
+# Install custom exception handlers
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
