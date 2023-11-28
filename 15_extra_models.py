97fcead4bbb46d4d14760fa93f38cf9b8b8098b2
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 13:30:43 +0900

    Reduce duplication

--- 15_extra_models.py
+++ 15_extra_models.py
@@ -4,24 +4,22 @@ from pydantic import BaseModel, EmailStr
 app = FastAPI()
 
 
-class UserIn(BaseModel):
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: str | None = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
+class UserOut(UserBase):
+    pass
 
 
-class UserInDB(BaseModel):
-    username: str
+class UserInDB(UserBase):
     hashed_password: str
-    email: EmailStr
-    full_name: str | None = None
 
 
 def fake_password_hasher(raw_password: str):
@@ -30,14 +28,13 @@ def fake_password_hasher(raw_password: str):
 
 def fake_save_user(user_in: UserIn):
     hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.model_dump(), hashed_password=hashed_password)
-    print(f"User saved! ..not really {user_in_db=}")
-    # UserInDB(username='string', hashed_password='supersecretstring', email='user@example.com', full_name='string')
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
     return user_in_db
 
 
-# Multiple models
-@app.put("/user/", response_model=UserOut)
+# Multiple models |  Reduce duplication
+@app.post("/user/", response_model=UserOut)
 async def create_user(user_in: UserIn):
     user_saved = fake_save_user(user_in)
     return user_saved
