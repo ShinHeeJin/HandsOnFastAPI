e145a1e5737b0487b0d7acd8d04b6f663d4545cb
ShinHeeJin <sisesu92@gmail.com>
2023-11-28 17:11:27 +0900

    Optional File Upload

--- 18_request_files.py
+++ 18_request_files.py
@@ -7,24 +7,31 @@ app = FastAPI()
 
 # Define File Parameters
 @app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
+async def create_file(file: Annotated[bytes, File()] = None):
+    if not file:
+        return {"message": "No file sent"}
     return {"file_size": len(file)}
 
 
 # File Parameters with UploadFile
 @app.post("/uploadfild/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile = None):
     """
-    # Using UploadFile has several advantages over bytes:
+    ### 1. Using UploadFile has several advantages over bytes:
 
     1. You don't have to use File() in the default value of the parameter.
     2. It uses a "spooled" file:
         * A file stored in memory up to a maximum size limit, and after passing this limit it will be stored in disk.
     3. This means that it will work well for large files like images, videos, large binaries, etc. without consuming all the memory.
 
-    # UploadFile has the following async methods.
+    ### 2. UploadFile has the following async methods.
     As all these methods are async methods, you need to "await" them.
+
+    ```python
     content = await file.read()
+    ```
     """
     print(f"{type(file)=}")  # =<class 'starlette.datastructures.UploadFile'>
+    if not file:
+        return {"message": "No upload file sent"}
     return {"filename": file.filename}
