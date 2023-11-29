1e3043d59c7d76c4f9fa29a073e8fbf63261a877
ShinHeeJin <sisesu92@gmail.com>
2023-11-29 11:00:27 +0900

    add tags to your path operation

---
+++ 21_path_peration_configuration.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+
+
+# add tags to your path operation
+@app.post("/items/", response_model=Item, tags=["items"])
+async def create_item(item: Item):
+    return item
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def resd_users():
+    return [{"username": "johndeo"}]
