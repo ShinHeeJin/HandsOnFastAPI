3fb6e1bedaaeb13b1bfc56713b1caac8bf227b36
ShinHeeJin <sisesu92@gmail.com>
2023-11-29 11:52:48 +0900

    Using Pydantic's update parameter

--- 23_body_updates.py
+++ 23_body_updates.py
@@ -49,3 +49,24 @@ async def update_item(item_id: str, item: Item):
     update_item_encoded = jsonable_encoder(item)
     items[item_id] = update_item_encoded
     return update_item_encoded
+
+
+# Using Pydantic's update parameter
+@app.patch("/items2/{item_id}", response_model=Item)
+async def update_item2(item_id: str, item: Item):
+    # item = Item(name='updated foo', description=None, price=None, tax=10.5, tags=[])
+
+    stored_item_data = items[item_id]
+    # stored_item_data = {'name': 'Foo', 'price': 50.2}
+
+    stored_item_model = Item(**stored_item_data)
+    # stored_item_model = Item(name='Foo', description=None, price=50.2, tax=10.5, tags=[])
+
+    update_data = item.model_dump(exclude_unset=True)
+    # update_data = {'name': 'updated foo'}
+
+    updated_item = stored_item_model.model_copy(update=update_data)
+    # updated_item = Item(name='updated foo', description=None, price=50.2, tax=10.5, tags=[])
+
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
