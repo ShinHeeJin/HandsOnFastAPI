7fb25fb594ff4e19c693f8ad19962fee9562a27d
ShinHeeJin <sisesu92@gmail.com>
2023-11-30 22:41:18 +0900

    Returning a custom Response

--- advanced_user_guide/3_return_a_response_directly.py
+++ advanced_user_guide/3_return_a_response_directly.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Response, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -23,3 +23,19 @@ def update_item(id: str, item: Item):
     when you return a Response, FastAPI will pass it directly. It won't do any data conversion with Pydantic models
     """
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder(item))
+
+
+# Returning a custom Response
+@app.get("/legacy/")
+def get_legacy_data():
+    data = """<?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type="application/xml")
