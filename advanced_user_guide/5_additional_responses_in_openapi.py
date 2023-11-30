75a4e398b4f8334d8f5d9247f8e6942063327427
ShinHeeJin <sisesu92@gmail.com>
2023-12-01 01:11:58 +0900

    Additional media types for the main response

--- advanced_user_guide/5_additional_responses_in_openapi.py
+++ advanced_user_guide/5_additional_responses_in_openapi.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 from pydantic import BaseModel
 
 
@@ -20,3 +20,21 @@ async def read_item(item_id: str):
     if item_id == "foo":
         return {"id": "foo", "value": "there goes my hero"}
     return JSONResponse(status_code=404, content={"message": "Item not found"})
+
+
+# Additional media types for the main response
+@app.get(
+    "/items2/{item_id}",
+    response_model=Item,
+    responses={
+        200: {"content": {"image/png": {}}, "description": "Return the JSON item or an image."}
+    },
+)
+async def read_item2(item_id: str, img: bool | None = None):
+    """
+    You can use this same responses parameter to add different media types for the same main response.
+    * Notice that you have to return the image using a FileResponse directly.
+    """
+    if img:
+        return FileResponse("image.png", media_type="image/png")
+    return {"id": item_id, "value": "there goes ym hero"}
