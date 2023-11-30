59ec3a939bd77a02e78abdde4279076915e31900
ShinHeeJin <sisesu92@gmail.com>
2023-12-01 01:46:38 +0900

    Return a Response directly

--- advanced_user_guide/7_response_headers.py
+++ advanced_user_guide/7_response_headers.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+
+class Message(BaseModel):
+    message: str = Field(
+        title="message", description="This is Success Message", examples=["success!!"]
+    )
 
 
 # Use a Response parameter
@@ -16,3 +24,20 @@ async def get_headers(response: Response):
     """
     response.headers["X-Cat-Dog"] = "alone in the world"
     return {"message": "Hello world"}
+
+
+# Return a Response directly
+@app.get(
+    "/headers/",
+    responses={
+        200: {
+            "model": Message,
+            "description": "hello world!!",
+            "content": {"application/json": {"example": {"message": "Hello world"}}},
+        },
+    },
+)
+def get_headers2():
+    content = {"message": "Hello world"}
+    headers = {"X-Cat-Dog": "alone in the world", "Content-Language": "en-US"}
+    return JSONResponse(content=content, headers=headers)
