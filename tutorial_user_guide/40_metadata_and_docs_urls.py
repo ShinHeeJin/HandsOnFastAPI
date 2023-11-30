cbbcfa87fe3679d37a8e76d0a6ebae5afa34cde3
ShinHeeJin <sisesu92@gmail.com>
2023-11-30 21:04:19 +0900

    Create metadata for tags

--- tutorial_user_guide/40_metadata_and_docs_urls.py
+++ tutorial_user_guide/40_metadata_and_docs_urls.py
@@ -17,6 +17,21 @@ You will be able to:
 * **Read users** (_not implemented_).
 """
 
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
 app = FastAPI(
     title="ChimichangApp",
     description=description,
@@ -32,9 +47,15 @@ app = FastAPI(
         "name": "Apache 2.0",
         "identifier": "MIT",
     },
+    openapi_tags=tags_metadata,
 )
 
 
-@app.get("/items/")
-async def read_items():
-    return [{"name": "Katana"}]
+@app.get("/users/", tags=["users"])
+async def get_users():
+    return [{"name": "Harry"}, {"name": "Ron"}]
+
+
+@app.get("/items/", tags=["items"])
+async def get_items():
+    return [{"name": "wand"}, {"name": "flying broom"}]
