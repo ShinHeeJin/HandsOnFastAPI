07c00d92918905e3bcba7a0df45d283cb12a5656
ShinHeeJin <sisesu92@gmail.com>
2023-12-01 01:37:45 +0900

    Return a Response directly

--- advanced_user_guide/6_response_cookies.py
+++ advanced_user_guide/6_response_cookies.py
@@ -1,6 +1,8 @@
 # Use a Response parameter
+from datetime import timedelta
 
 from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -10,3 +12,14 @@ app = FastAPI()
 def create_cookie(response: Response):
     response.set_cookie(key="fakesession", value="fake-cookie-session-value")
     return {"message": "Come to the dark side, we have cookies"}
+
+
+# Return a Response directly
+@app.post("/cookie")
+def create_cookie2():
+    content = {"message": "Come to the dark side, we have cookies"}
+    response = JSONResponse(content=content)
+    response.set_cookie(
+        key="fakesession", value="fake-cookie-session-value", expires=timedelta(days=1)
+    )
+    return response
