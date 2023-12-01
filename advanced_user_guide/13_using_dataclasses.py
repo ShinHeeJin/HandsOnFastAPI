4c2a178466540798c61a1a1b5e5325bc3b266c54
ShinHeeJin <sisesu92@gmail.com>
2023-12-01 18:36:48 +0900

    Dataclasses in Nested Data Structures

--- advanced_user_guide/13_using_dataclasses.py
+++ advanced_user_guide/13_using_dataclasses.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from fastapi import FastAPI
 
@@ -36,3 +36,51 @@ async def create_item(item: Item):
     FastAPI is using Pydantic to convert those standard dataclasses to Pydantic's own flavor of dataclasses.
     """
     return item
+
+
+@dataclass
+class Item2:
+    name: str
+    description: str | None = None
+
+
+@dataclass
+class Author:
+    name: str
+    items: list[Item2] = field(default_factory=list)
+
+
+# Dataclasses in Nested Data Structures
+@app.post("/authors/{author_id}/items/", response_model=Author)  #
+async def create_author_items(author_id: str, items: list[Item2]):  #
+    return {"name": author_id, "items": items}  #
+
+
+@app.get("/authors/", response_model=list[Author])  #
+def get_authors():  #
+    return [  #
+        {
+            "name": "Breaters",
+            "items": [
+                {
+                    "name": "Island In The Moon",
+                    "description": "A place to be be playin' and havin' fun",
+                },
+                {"name": "Holy Buddies"},
+            ],
+        },
+        {
+            "name": "System of an Up",
+            "items": [
+                {
+                    "name": "Salt",
+                    "description": "The kombucha mushroom people's favorite",
+                },
+                {"name": "Pad Thai"},
+                {
+                    "name": "Lonely Night",
+                    "description": "The mostests lonliest nightiest of allest",
+                },
+            ],
+        },
+    ]
