6c04a2c5e524ef2561bb111397bd584d49dada30
ShinHeeJin <sisesu92@gmail.com>
2023-12-09 20:20:39 +0900

    Setting the root_path in the FastAPI app

--- advanced_user_guide/16_behind_a_proxy.py
+++ advanced_user_guide/16_behind_a_proxy.py
@@ -11,11 +11,16 @@ And it's also used internally when mounting sub-applications.
 
 from fastapi import FastAPI, Request
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
 
 # uvicorn main:app --root-path /api/v1
 @app.get("/app")
 def read_main(request: Request):
+    """
+    But if you go with your browser to http://127.0.0.1:8000/app you will see the normal response:
+    So, it won't expect to be accessed at http://127.0.0.1:8000/api/v1/app.
+    Uvicorn will expect the proxy to access Uvicorn at http://127.0.0.1:8000/app, and then it would be the proxy's responsibility to add the extra /api/v1 prefix on top.
+    """
     root_path = request.scope.get("root_path")  # "/api/v1"
     return {"message": "Hello World", "root_path": root_path}
